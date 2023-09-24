c85f97b6762741089db4c52550409f7a1bb4abbb
GustavoRocha2050 <112986519+GustavoRocha2050@users.noreply.github.com>
2023-09-24 09:15:42 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,3 @@
+
+var nome = window.prompt('Olá! Você é um usuário light ou premium?') // Janela perguntando nome
+window.alert('É um grande prazer reve-lo usuário '  + nome + ', desfrute dos benéficios do seu plano!')
